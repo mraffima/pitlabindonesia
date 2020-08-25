6086ca119d5b240fd82ee6583d3eadddfa1c7bef
mraffima <muhammad.raffi36@gmail.com>
2020-08-25 13:12:54 +0700

    Upload - Progress full frontend

---
+++ find_us.php
@@ -0,0 +1,116 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Find Us</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+   <!-- Favicons -->
+   <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: OnePage - v2.0.0
+  * Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style type="text/css">
+  	body{
+  		background-color: #000;
+  	}
+  </style>
+  </head>
+  <body>
+  	<div class="text-center">
+	  <img src="assets/img/icon.png" class="img-fluid" alt="Responsive image">
+	</div>
+
+	<div>
+
+	</div>
+    <!-- <div class="container">	
+    	<div class="border border-dark">
+		  <div class="row">
+		    <div class="col align-self-center border">
+		    	<center>Car/ Bike Wash</center>
+		    </div>
+	  	</div>
+	  	<div class="row">
+		    <div class="col align-self-center border">
+		      <center>Coating</center>
+		    </div>
+	  	</div>
+	  	<div class="row">
+		    <div class="col align-self-center border">
+		      <center>Paint Protection</center>
+		    </div>
+	  	</div>
+	  	<div class="row">
+		    <div class="col align-self-center border">
+		      <center>Body Repair</center>
+		    </div>
+	  	</div>
+	  	<div class="row">
+		    <div class="col align-self-center border">
+		      <center>Repaint</center>
+		    </div>
+	  	</div>
+	  	<div class="row">
+		    <div class="col align-self-center border">
+		      <center>Restoration</center>
+		    </div>
+	  	</div>
+	  	<div class="row">
+		    <div class="col align-self-center border">
+		      <center>Sticker Wrappingn</center>
+		    </div>
+	  	</div>
+	  </div>
+	</div> -->
+	<center>
+		<div class="copyright">
+		  &copy; Copyright <strong><span>2020</span></strong>. All Rights Reserved
+		</div>	
+	</center>
+    
+
+    <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  </body>
+</html>
